5ff9c149a7266ad89b8e1657bd9a9d83518f745f
SteveOscar <steveoscaro@gmail.com>
2021-02-07 18:36:36 -0800

    adds css foundation with global styles

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,9 +1,58 @@
 import Header from './Header';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2');
+    format('woff2');
+    font-weight: normal;
+    font-style: normal:
+  }
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --lightGrey: #E1E1E1;
+    --offWhite: #EDEDED;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0,0.9);
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'radnika_next', --apple-system;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button {
+    font-family: 'radnika_next', --apple-system;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`
 
 export default function Page({children, cool}) {
   return <div>
+    <GlobalStyles />
     <Header></Header>
-    <h1>I am page</h1>
-    {children}
+    <InnerStyles>
+      {children}
+      </InnerStyles>
   </div>
 }
